0b98450284ca3f264b939565fd2aa28707660593
Alvin Ng and Tony Fabery <alvin+tony@>
2016-09-20 15:20:37 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+question_list = [
+  ["eat cheese or salami for the rest of your life?", "cheese", "salami"],
+  ["be able to dunk a basketball or hit a home run?", "dunk", "homer"],
+  ["watch seinfeld or frasier?", "seinfeld", "frasier"],
+  ["read minds or fly?", "read minds", "fly"],
+  ["have a third nostril or third ear?", "nostril", "ear"],
+  ["get a bowl of dandruff or fingernail clippings", "dandruff", "clippings"]
+]
+
+question_list.each do |prompt, option1, option2|
+  Question.create(prompt: prompt, option1: option1, option2: option2)
+end
